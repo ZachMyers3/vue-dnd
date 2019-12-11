5aba0806ea1ee281ab82b53c0d754175135355f0
Zach Myers <zachmyers@woosterbrush.com>
2019-12-11 17:27:34 -0500

    cleaned up repo

--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface PlayerCharacter {
-    firstName: string,
-    lastName: string,
-    maxHitPoints: number,
-    currentHitPoints: number,
-    baseAC: number,
-    currentAC: number
-}
