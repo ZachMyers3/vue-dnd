51f6e06807191e4b6d07f40d0dfa24b1ed97c059
Zach Myers <zachmyers@woosterbrush.com>
2020-01-08 16:57:58 -0500

    tuned imports on router, improving webpack

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,12 +1,20 @@
 import VueRouter from 'vue-router'
-import Monsters from '@/views/Monsters.vue'
-import Characters from '@/views/Characters.vue'
-import Spells from '@/views/Spells.vue'
-import CharacterInfo from '@/views/CharacterInfo.vue'
-import MonsterInfo from '@/views/MonsterInfo.vue'
-import SpellInfo from '@/views/SpellInfo.vue'
-import Equipment from '@/views/Equipment.vue'
-import EquipmentInfo from '@/views/EquipmentInfo.vue'
+// import Monsters from '@/views/Monsters.vue'
+// import Characters from '@/views/Characters.vue'
+// import Spells from '@/views/Spells.vue'
+// import CharacterInfo from '@/views/CharacterInfo.vue'
+// import MonsterInfo from '@/views/MonsterInfo.vue'
+// import SpellInfo from '@/views/SpellInfo.vue'
+// import Equipment from '@/views/Equipment.vue'
+// import EquipmentInfo from '@/views/EquipmentInfo.vue'
+const Characters = () => import('@/views/Characters.vue')
+const CharacterInfo = () => import('@/views/CharacterInfo.vue')
+const Monsters = () => import('@/views/Monsters.vue')
+const MonsterInfo = () => import('@/views/MonsterInfo.vue')
+const Spells = () => import('@/views/Spells.vue')
+const SpellInfo = () => import('@/views/SpellInfo.vue')
+const Equipment = () => import('@/views/Equipment.vue')
+const EquipmentInfo = () => import('@/views/EquipmentInfo.vue')
 
 const characterPrefix = 'character'
 
