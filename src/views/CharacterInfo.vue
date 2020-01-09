3b0bdf64056f2d334a913d69fc7035c0938b003e
Zach Myers <zachmyers@woosterbrush.com>
2020-01-09 08:55:40 -0500

    added available spells to characterinfo

--- src/views/CharacterInfo.vue
+++ src/views/CharacterInfo.vue
@@ -19,7 +19,83 @@
 
             <!-- Casting and Components -->
             <v-list-item three-line class="text-left">
-            </v-list-item> 
+                <v-list-item-content>
+                    <div class="overline mb-4">STATS</div>
+                    <div class="casting">
+                    <v-simple-table>
+                        <tbody>
+                            <tr>
+                                <td>Strength</td>
+                                <td>{{ c.strength }}</td>
+                            </tr>
+                            <tr>
+                                <td>Dexterity</td>
+                                <td>{{ c.dexterity }}</td>
+                            </tr>
+                            <tr>
+                                <td>Constitution</td>
+                                <td>{{ c.constitution }}</td>
+                            </tr>
+                            <tr>
+                                <td>Wisdom</td>
+                                <td>{{ c.wisdom }}</td>
+                            </tr>
+                            <tr>
+                                <td>Charisma</td>
+                                <td>{{ c.charisma }}</td>
+                            </tr>
+                        </tbody>
+                    </v-simple-table>
+                    </div>
+                </v-list-item-content>
+                <v-list-item-content>
+                    <div class="overline mb-4">INFORMATION</div>
+                    <v-simple-table>
+                        <tbody>
+                            <tr>
+                                <td>Eyes</td>
+                                <td>{{ c.Eyes }}</td>
+                            </tr>
+                            <tr>
+                                <td>Hair</td>
+                                <td>{{ c.Hair }}</td>
+                            </tr>
+                            <tr>
+                                <td>Height</td>
+                                <td>{{ c.Height }}</td>
+                            </tr>
+                            <tr>
+                                <td>Weight</td>
+                                <td>{{ c.Weight }}</td>
+                            </tr>
+                            <tr>
+                                <td>Background</td>
+                                <td>{{ c.background }}</td>
+                            </tr>
+                        </tbody>
+                    </v-simple-table>
+                </v-list-item-content>
+            </v-list-item>
+            <v-list-item three-line class="text-left">
+                <v-list-item-content>
+                    <div class="overline mb-4">AVAILABLE SPELLS</div>
+                    <v-data-table
+                        :headers="spell_headers"
+                        :items="c.spells"
+                        :item-key="c.spells.id"
+                        :search="search"
+                        dense
+                    >
+                        <template v-slot:item.name="{ item }">
+                            <div class="name">
+                                <router-link :to="{ name: 'spellInfo', params: { id: item.id }}">
+                                {{ item.name }}
+                                </router-link>
+                            </div>
+                        </template>
+                    </v-data-table>
+                </v-list-item-content>
+            </v-list-item>
         </v-card>
     </div>
     <p v-else>Loading...</p>
@@ -38,6 +114,11 @@ export default class CharacterInfo extends Vue {
     @Prop() private id!: string;
     private c!: Character;
     private loading: boolean = false;
+    private search: string = '';
+    private spell_headers: any[] = [
+        { text: 'Name', value: 'name' },
+        { text: 'Level', value: 'level' }
+    ];
 
     async mounted():Promise<void> {
       this.loading = !this.loading;
@@ -54,4 +135,10 @@ export default class CharacterInfo extends Vue {
 </script>
 
 <style lang="scss">
+div.name {
+  a {
+    color: #2c3e50;
+    text-decoration: none;
+  }
+}
 </style>
