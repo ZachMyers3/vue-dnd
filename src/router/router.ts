f2f75a3a83e92dfbf9b49abac3a30b5fed785018
Zach Myers <zachmyers@woosterbrush.com>
2020-06-08 14:27:40 -0400

    added web chunking

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,76 +1,81 @@
-import VueRouter from 'vue-router'
-// import Monsters from '@/views/Monsters.vue'
-// import Characters from '@/views/Characters.vue'
-// import Spells from '@/views/Spells.vue'
-// import CharacterInfo from '@/views/CharacterInfo.vue'
-// import MonsterInfo from '@/views/MonsterInfo.vue'
-// import SpellInfo from '@/views/SpellInfo.vue'
-// import Equipment from '@/views/Equipment.vue'
-// import EquipmentInfo from '@/views/EquipmentInfo.vue'
-const Characters = () => import('@/views/Characters.vue')
-const CharacterInfo = () => import('@/views/CharacterInfo.vue')
-const Monsters = () => import('@/views/Monsters.vue')
-const MonsterInfo = () => import('@/views/MonsterInfo.vue')
-const Spells = () => import('@/views/Spells.vue')
-const SpellInfo = () => import('@/views/SpellInfo.vue')
-const Equipment = () => import('@/views/Equipment.vue')
-const EquipmentInfo = () => import('@/views/EquipmentInfo.vue')
+import VueRouter from "vue-router";
 
-const characterPrefix = 'character'
+const characterPrefix = "character";
 
 const routes = [
-    { // any undefined route will redirect to name characterInfo
-		path: '*',
-		redirect: { name: 'characters' }
-    },
-    { 
-        name: 'characters',
-        path: '/characters',
-        component: Characters
-    },
-    { // characterInfo route will use view PlayerCharacter
-        name: 'characterInfo',
-        path: `/${characterPrefix}/:id`,
-        props: true,
-        component: CharacterInfo
-    },
-    {
-        name: 'monsters',
-        path: '/monsters',
-        component: Monsters
-    },
-    { // characterInfo route will use view PlayerCharacter
-        name: 'monsterInfo',
-        path: `/monster/:id`,
-        props: true,
-        component: MonsterInfo
-    },
-    {
-        name: 'spells',
-        path: '/spells',
-        component: Spells
-    },
-    {
-        name: 'spellInfo',
-        path: `/spell/:id`,
-        props: true,
-        component: SpellInfo
-    },
-    {
-        name: 'equipment',
-        path: '/equipment',
-        component: Equipment
-    },
-    {
-        name: 'equipmentInfo',
-        path: `/equipment/:id`,
-        props: true,
-        component: EquipmentInfo
-    }
-]
+  {
+    // any undefined route will redirect to name characterInfo
+    path: "*",
+    redirect: { name: "characters" },
+  },
+  {
+    name: "characters",
+    path: "/characters",
+    component: () =>
+      import(/* webpackChunkName: "Characters" */ "@/views/Characters.vue"),
+  },
+  {
+    // characterInfo route will use view PlayerCharacter
+    name: "characterInfo",
+    path: `/${characterPrefix}/:id`,
+    props: true,
+    component: () =>
+      import(
+        /* webpackChunkName: "CharacterInfo" */ "@/views/CharacterInfo.vue"
+      ),
+  },
+  {
+    name: "monsters",
+    path: "/monsters",
+    component: () =>
+      import(/* webpackChunkName: "Monsters" */ "@/views/Monsters.vue"),
+  },
+  {
+    // characterInfo route will use view PlayerCharacter
+    name: "monsterInfo",
+    path: `/monster/:id`,
+    props: true,
+    component: () =>
+      import(/* webpackChunkName: "MonsterInfo" */ "@/views/MonsterInfo.vue"),
+  },
+  {
+    name: "spells",
+    path: "/spells",
+    component: () =>
+      import(/* webpackChunkName: "Spells" */ "@/views/Spells.vue"),
+  },
+  {
+    name: "spellInfo",
+    path: `/spell/:id`,
+    props: true,
+    component: () =>
+      import(/* webpackChunkName: "SpellInfo" */ "@/views/SpellInfo.vue"),
+  },
+  {
+    name: "equipment",
+    path: "/equipment",
+    component: () =>
+      import(/* webpackChunkName: "Equipment" */ "@/views/Equipment.vue"),
+  },
+  {
+    name: "equipmentInfo",
+    path: `/equipment/:id`,
+    props: true,
+    component: () =>
+      import(
+        /* webpackChunkName: "EquipmentInfo" */ "@/views/EquipmentInfo.vue"
+      ),
+  },
+  {
+    name: "map",
+    path: `/map`,
+    props: true,
+    component: () => import(/* webpackChunkName: "Map" */ "@/views/Map.vue"),
+  },
+];
 
 export const router = new VueRouter({
-    mode: 'history',
-    routes,
-    linkActiveClass: 'is-active'
-})
+  mode: "history",
+  routes,
+  linkActiveClass: "is-active",
+});
