b9fd023fd5429d01ee2be4c689d754e45e4c2144
Zach Myers <zachmyers@woosterbrush.com>
2020-06-08 15:18:12 -0400

    added vue-panzoom

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import { router } from './router/router'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import { router } from "./router/router";
 
-import vuetify from './plugins/vuetify'
+import vuetify from "./plugins/vuetify";
+import panZoom from "vue-panzoom";
 
-import App from './App.vue'
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(panZoom);
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
