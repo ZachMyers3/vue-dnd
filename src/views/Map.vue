829ac5f409845d7c4e334cbba6c63dc87a5ed9fc
Zach Myers <zachmyers3@gmail.com>
2020-06-08 21:17:01 -0400

    added map asset, adjusted config for panzoom

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,5 +1,14 @@
 <template>
-  <panZoom>
-    <img src="@/assets/logo.png" alt="" height="30px" />
-  </panZoom>
+  <v-card class="map-wrapper">
+    <panZoom :options="{minZoom: 0.5, maxZoom: 8, bounds: true}">
+      <img src="@/assets/map.png" alt />
+    </panZoom>
+  </v-card>
 </template>
+
+<style lang="scss">
+img {
+  height: 100%;
+  width: 100%;
+}
+</style>
